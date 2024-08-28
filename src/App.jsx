96556384dc9693802d7694e2c8db189420dc25db
Valentin Thibault <valentin.thlt@gmail.com>
2024-08-28 15:14:03 +0200

    feat(components): add reusable FormInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { FormInput } from './components/FormInput'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <form>
+      <FormInput type='text' name='Lastname' isRequired />
+    </form>
   )
 }
 
